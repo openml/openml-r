64f63f3ab4789260db7ee18f8be1fc3c4e34e361
Dominik Kirchhoff <dominik.kirchhoff@tu-dortmund.de>
2013-12-28 01:26:56 +0100

    documentation

--- openML/R/createOpenMLImplementationForMLRLearner.R
+++ openML/R/createOpenMLImplementationForMLRLearner.R
@@ -1,3 +1,19 @@
+#' createOpenMLImplementationForMLRLearner.
+#'
+#' Create an OpenML implementation description object for an mlr learner. 
+#' Required if you want to upload an mlr learner.
+#' 
+#' @param lrn [\code{\link[mlr]{Learner}}]\cr 
+#'   The mlr learner.
+#' @param name [\code{character(1)}]\cr 
+#'   The name of the implementation object. Default is the learner's ID.
+#' @param version [\code{character(1)}]\cr 
+#'   The version of the implementation. Default is the version of the package.
+#' @param description [\code{character(1)}]\cr
+#'   An optional description of the learner. 
+#'   Default is a short specification of the learner and the associated package.
+#' @return [\code{\link{OpenMLImplementation}}]. 
+#' @export
 createOpenMLImplementationForMLRLearner <- function(
   lrn, 
   name = lrn$id, 
