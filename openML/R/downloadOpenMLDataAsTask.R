52c28afbbe1c7e67c275101228ddf018bd022b5e
Dominik Kirchhoff <dominik.kirchhoff@tu-dortmund.de>
2014-02-04 16:22:30 +0100

    arg checks and small adjustments

--- openML/R/downloadOpenMLDataAsTask.R
+++ openML/R/downloadOpenMLDataAsTask.R
@@ -1,25 +1,26 @@
 #' @export
 downloadOpenMLDataAsMlrTask <- function(name, dir = tempdir(), clean.up = TRUE, show.info = TRUE) {
-  #FIXME: more arg checks
   checkArg(name, "character", len = 1L, na.ok = FALSE)
-
+  checkArg(dir, "character", len = 1L, na.ok = FALSE)
+  checkArg(clean.up, "logical", len = 1L, na.ok = FALSE)
+  checkArg(show.info, "logical", len = 1L, na.ok = FALSE)
+  
   fn.data.set.desc <- file.path(dir, "data_set_description.xml")
   fn.data.set <- file.path(dir, "data_set.ARFF")
 
   # get id for given dataset name
-  SQL <- paste0("SELECT did, default_target_attribute FROM dataset WHERE name = '",name,"'")
+  SQL <- paste0("SELECT did, default_target_attribute FROM dataset WHERE name = '", name, "'")
   sqlRes <- runSQLQuery(SQL)
-  id <- sqlRes[1,"did"]
-  id <- as.integer(as.character(id))
-  target <- as.character(sqlRes[1,"default_target_attribute"])
+  id <- sqlRes[1, "did"]
+  target <- sqlRes[1,"default_target_attribute"]
 
   if (show.info) {
-    messagef("Downloading task %s from OpenML repository.", name)
+    messagef("Downloading data set '%s' from OpenML repository.", name)
     messagef("Intermediate files (XML and ARFF) will be stored in : %s", dir)
   }
 
   downloadOpenMLDataSetDescription(id = id, file = fn.data.set.desc, show.info = show.info)
-  task.data.desc <- parseOpenMLDataSetDescription(file=fn.data.set.desc)
+  task.data.desc <- parseOpenMLDataSetDescription(file = fn.data.set.desc)
   downloadOpenMLDataSet(task.data.desc@url, fn.data.set, show.info)
   task.data.desc@data.set <- parseOpenMLDataSet(task.data.desc, fn.data.set)
 
@@ -30,11 +31,11 @@ downloadOpenMLDataAsMlrTask <- function(name, dir = tempdir(), clean.up = TRUE, 
       messagef("All intermediate XML and ARFF files are now removed.")
   }
 
-  #FIXME: Dirty from now on:
-  classif <- is.factor(task.data.desc@data.set[,target])
+  # FIXME: Dirty from now on:
+  classif <- is.factor(task.data.desc@data.set[, target])
 
   data <- task.data.desc@data.set
-  #FIXME: some data sets have empty factor levels, mlr does not like this
+  # FIXME: some data sets have empty factor levels, mlr does not like this
   # fix this for now by removing
   data <- droplevels(data)
 
@@ -51,6 +52,5 @@ downloadOpenMLDataAsMlrTask <- function(name, dir = tempdir(), clean.up = TRUE, 
     mlr.task <- makeRegrTask(id = name, data = data, target = target)
   }
 
-  mlr.task
-
+  return(mlr.task)
 }
