07de66e8e356295415576e695fce3ad9c6f31be8
Dominik Kirchhoff <dominik.kirchhoff@tu-dortmund.de>
2014-04-16 18:34:12 +0200

    unit tests

---
+++ inst/tests/test_runSQLQuery.R
@@ -0,0 +1,6 @@
+context("runSQLQuery")
+
+test_that("runSQLQuery", {
+  res <- runSQLQuery("SELECT * FROM task")
+  expect_true(is.data.frame(res) && nrow(res) > 1  && ncol(res) == 2 && is.numeric(res$task_id))
+})  
