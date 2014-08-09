840886ccdabf436c7d94c630d46999d22562fba6
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-09 18:31:13 +0200

    clean up

--- R/server_interface.R
+++ R/server_interface.R
@@ -1,18 +1,14 @@
-# Old Server:
-# OPEN_ML_SERVER_API_URL = "http://expdb.cs.kuleuven.be/expdb/api"
-# Beta Server:
-OPEN_ML_SERVER_API_URL = "http://openml.liacs.nl/api"
+OPEN_ML_SERVER_API_URL = "http://openml.org/api"
 
 # Generate a URL to download files from the server API.
-# 
+#
 # @param fname [\code{character(1)}]\cr
 #   Name of API function to call on server.
 # @param ... [any]\cr
 #   Arguments for API call.
 # @return [\code{character(1)}].
-# @seealso \code{\link{downloadAPICallFile}}, \code{\link{downloadBinaryFile}}
-# 
-# FIXME use getForm?
+#
+# FIXME: use getForm?
 getServerFunctionURL = function(fname, ...) {
   url = sprintf("%s/?f=%s", OPEN_ML_SERVER_API_URL, fname)
   args = list(...)
