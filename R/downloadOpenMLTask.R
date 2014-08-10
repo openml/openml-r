7648c139a6fc203b4cf75b8a2622d1165efa9277
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-10 18:49:43 +0200

    clean up

--- R/downloadOpenMLTask.R
+++ R/downloadOpenMLTask.R
@@ -35,8 +35,8 @@
 #' \dontrun{
 #' task = downloadOpenMLTask(id = 1)
 #' show(task)
-#' print(task$task.type)
-#' print(task$task.target.features)
+#' print(task$type)
+#' print(task$target.features)
 #' print(head(task$data.desc$data.set))
 #' }
 
@@ -79,30 +79,30 @@ downloadOpenMLTask = function(id, dir = tempdir(), clean.up = TRUE,
   task = parseOpenMLTask(fn.task)
 
   if (fetch.data.set.description) {
-    downloadOpenMLDataSetDescription(task$task.data.desc.id, fn.data.set.desc, show.info)
-    task$task.data.desc = parseOpenMLDataSetDescription(fn.data.set.desc)
+    downloadOpenMLDataSetDescription(task$data.desc.id, fn.data.set.desc, show.info)
+    task$data.desc = parseOpenMLDataSetDescription(fn.data.set.desc)
   }
 
   if (fetch.data.set) {
-    downloadOpenMLDataSet(task$task.data.desc$url, fn.data.set, show.info)
-    task$task.data.desc$data.set = parseOpenMLDataSet(task$task.data.desc, fn.data.set)
+    downloadOpenMLDataSet(task$data.desc$url, fn.data.set, show.info)
+    task$data.desc$data.set = parseOpenMLDataSet(task$data.desc, fn.data.set)
 
     # make valid column names
-    task$task.data.desc$original.col.names = colnames(task$task.data.desc$data.set)
-    task$task.data.desc$new.col.names = make.names(task$task.data.desc$original.col.names, unique = TRUE)
-    target.inds = which(task$task.data.desc$original.col.names %in% task$task.target.features )
-    task$task.target.features = task$task.data.desc$new.col.names[target.inds]
-    colnames(task$task.data.desc$data.set) = task$task.data.desc$new.col.names
+    task$data.desc$original.col.names = colnames(task$data.desc$data.set)
+    task$data.desc$new.col.names = make.names(task$data.desc$original.col.names, unique = TRUE)
+    target.inds = which(task$data.desc$original.col.names %in% task$target.features )
+    task$target.features = task$data.desc$new.col.names[target.inds]
+    colnames(task$data.desc$data.set) = task$data.desc$new.col.names
   }
 
   if (fetch.data.splits) {
     # No real error handling. If no data splits are available, just print a warning and go on.
-    if (task$task.estimation.procedure$data.splits.url == "No URL") {
+    if (task$estimation.procedure$data.splits.url == "No URL") {
       warning("There is no URL to fetch data splits from.
         Either the task type does not support data splits or the task is defective.")
       } else {
-      downloadOpenMLDataSplits(task$task.estimation.procedure$data.splits.url, fn.data.splits, show.info)
-      task$task.estimation.procedure$data.splits = parseOpenMLDataSplits(task$task.data.desc$data.set, fn.data.splits)
+      downloadOpenMLDataSplits(task$estimation.procedure$data.splits.url, fn.data.splits, show.info)
+      task$estimation.procedure$data.splits = parseOpenMLDataSplits(task$data.desc$data.set, fn.data.splits)
     }
   }
 
@@ -120,8 +120,8 @@ parseOpenMLTask = function(file) {
   }
 
   # task
-  task.id = xmlRValI(doc, "/oml:task/oml:task_id")
-  task.type = xmlRValS(doc, "/oml:task/oml:task_type")
+  id = xmlRValI(doc, "/oml:task/oml:task_id")
+  type = xmlRValS(doc, "/oml:task/oml:task_type")
   targets = xmlValsMultNsS(doc, "/oml:task/oml:input/oml:data_set/oml:target_feature")
   params = getParams("oml:task")
 
@@ -133,7 +133,7 @@ parseOpenMLTask = function(file) {
   ns.preds.features = getNodeSet(doc, "/oml:task/oml:output/oml:predictions/oml:feature")
   preds.features = lapply(ns.preds.features, function(x) xmlGetAttr(x, "type"))
   names(preds.features) = sapply(ns.preds.features, function(x) xmlGetAttr(x, "name"))
-  task.preds = list(
+  preds = list(
     format = xmlRValS(doc, "/oml:task/oml:output/oml:predictions/oml:format"),
     features = preds.features
   )
@@ -155,15 +155,15 @@ parseOpenMLTask = function(file) {
   measures = xmlValsMultNsS(doc, "/oml:task/oml:input/oml:evaluation_measures/oml:evaluation_measure")
 
   task = makeOpenMLTask(
-    task.id = task.id,
-    task.type = task.type,
-    task.target.features = targets,
-    task.pars = params,
-    task.data.desc.id = data.desc.id,
-    task.data.desc = data.desc,
-    task.estimation.procedure = estim.proc,
-    task.preds = task.preds,
-    task.evaluation.measures = measures
+    id = id,
+    type = type,
+    target.features = targets,
+    pars = params,
+    data.desc.id = data.desc.id,
+    data.desc = data.desc,
+    estimation.procedure = estim.proc,
+    preds = preds,
+    evaluation.measures = measures
   )
   convertOpenMLTaskSlots(task)
 }
@@ -176,12 +176,12 @@ convertParam = function(params, name, fun) {
 
 convertOpenMLTaskSlots = function(task) {
   # convert estim params to correct types
-  p = task$task.estimation.procedure$parameters
+  p = task$estimation.procedure$parameters
   p = convertParam(p, "number_repeats", as.integer)
   p = convertParam(p, "number_folds", as.integer)
-  task$task.estimation.procedure$parameters = p
+  task$estimation.procedure$parameters = p
 
-  #task$task.evaluation.measures = strsplit(task$task.evaluation.measures, split = ",")[[1]]
+  #task$evaluation.measures = strsplit(task$evaluation.measures, split = ",")[[1]]
   return(task)
 }
 
