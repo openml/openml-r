d17f562c13c130a2fa2d5b615d15c3a5d11bb9be
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-11 12:19:49 +0200

    improve server tests

--- tests/testthat/test_serverall_download_all_datasets.R
+++ tests/testthat/test_serverall_download_all_datasets.R
@@ -2,7 +2,13 @@ context("download all data sets")
 
 test_that("download all data sets", {
   ids = getOpenMLDatasetNames()
-  for (i in ids) {
+
+  # remove some simulated data sets. they just take too long as they are large
+  j1 = str_detect(ids, "^BNG")
+  j2 = str_detect(ids, "^RandomRBF")
+  ids = ids[!(j1 | j2)]
+
+  for (i in ids[101:150]) {
     print(i)
     task = downloadOpenMLDataAsMlrTask(i, show.info = FALSE, clean.up = TRUE)
     ds = task$env$data
