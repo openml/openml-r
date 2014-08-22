35cc3cc44fa5ec2cb7c54c408d04d234634f7dfc
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-22 21:01:24 +0200

    clean up

--- R/runTask.R
+++ R/runTask.R
@@ -1,28 +1,28 @@
 #' Solve a task with MLR.
 #'
-#' Solve a task with a specified learner from MLR, compute predictions and upload the results 
+#' Solve a task with a specified learner from MLR, compute predictions and upload the results
 #' to the OpenML server.
-#' 
-#' @param task [\code{\linkS4class{OpenMLTask}}]\cr 
+#'
+#' @param task [\code{\linkS4class{OpenMLTask}}]\cr
 #'   An OpenML task. Required.
-#' @param learner [\code{\link[mlr]{Learner}}]\cr 
+#' @param learner [\code{\link[mlr]{Learner}}]\cr
 #'   Learner object from package mlr to solve the task.
-#' @param return.mlr.results [\code{logical(1)}]\cr 
-#'   Should not only the predictions but all of the by MLR computed information be returned? 
-#'   This includes test measures in each step of the resampling procedure as well as the aggregated 
+#' @param return.mlr.results [\code{logical(1)}]\cr
+#'   Should not only the predictions but all of the by MLR computed information be returned?
+#'   This includes test measures in each step of the resampling procedure as well as the aggregated
 #'   performance. See \code{\link[mlr]{resample}}. Default is \code{TRUE}.
 #' @param remove.const.features [\code{logical(1)}]\cr
-#'   Should constant features be removed? Default is \code{TRUE}. Note, that setting this 
-#'   to \code{FALSE} may lead to fatal errors. 
+#'   Should constant features be removed? Default is \code{TRUE}. Note, that setting this
+#'   to \code{FALSE} may lead to fatal errors.
 #' @param ... [any]\cr
-#'   Further arguments that are passed to \code{\link[mlr]{removeConstantFeatures}}. 
+#'   Further arguments that are passed to \code{\link[mlr]{removeConstantFeatures}}.
 #' @return List of:
 #'   \item{run.pred}{[\code{\link[mlr]{ResamplePrediction}}]\cr
 #'     Predictions resulting from the run. These are necessary in order to upload a run.}
 #'   \item{mlr.resample.results}{[\code{list}]\cr
 #'     The results of the MLR function \code{\link[mlr]{resample}}.\cr
-#'     \code{NULL} if \code{return.mlr.results == FALSE}.}  
-#' @seealso \code{\linkS4class{OpenMLTask}}, \code{\link[mlr]{learners}}, 
+#'     \code{NULL} if \code{return.mlr.results == FALSE}.}
+#' @seealso \code{\linkS4class{OpenMLTask}}, \code{\link[mlr]{learners}},
 #'   \code{\link{authenticateUser}}, \code{\link[mlr]{resample}}
 #' @export
 # FIXME: if !return.mlr.results, the output is not a list!
@@ -34,16 +34,16 @@ runTask = function(task, learner, return.mlr.results = FALSE, remove.const.feats
     (task$type == "Supervised Regression" && learner$type != "regr"))
     stopf("Learner type ('%s') does not correspond to task type ('%s').", task$type, learner$type)
   mlr.task = toMLR(task)
-  
+
   if (remove.const.feats)
-    mlr.task$mlr.task = removeConstantFeatures(x = mlr.task$mlr.task, ...)
-  
+    mlr.task$mlr.task = removeConstantFeatures(obj = mlr.task$mlr.task, ...)
+
   res = resample(learner, mlr.task$mlr.task, mlr.task$mlr.rin, measures = mlr.task$mlr.measures)
   pred = reformatPredictions(pred = res$pred$data, task = task)
   results = list(
-    run.pred = pred, 
+    run.pred = pred,
     mlr.resample.results = res
-  )  
+  )
   if (!return.mlr.results) {
     results$mlr.resample.results = NULL
     results = results$run.pred
@@ -55,23 +55,23 @@ runTask = function(task, learner, return.mlr.results = FALSE, remove.const.feats
 #
 # Reformat an MLR predictions data.frame, so that it fits the OpenML expectencies.
 #
-# @param pred [\code{\link[mlr]{Prediction}}]\cr 
+# @param pred [\code{\link[mlr]{Prediction}}]\cr
 #   MLR predictions data.frame.
-# @param task [\code{\linkS4class{OpenMLTask}}]\cr 
+# @param task [\code{\linkS4class{OpenMLTask}}]\cr
 #   The OpenML task to which the predictions belong.
 # @return data.frame with columns:
 #   \item{repeat}{[\code{numeric}}]\cr
 #     Current repetition of the estimation procedure.}
 #   \item{fold}{[\code{numeric}]\cr
-#     Current repetition of the estimation procedure.}  
+#     Current repetition of the estimation procedure.}
 #   \item{row_id}{[\code{numeric}]\cr
 #     The observation's row ID.}
 #   \item{prediction}{[\code{factor}]\cr
-#     The predicted class.}  
+#     The predicted class.}
 #   \item{confidence."classname"}{[\code{numeric}]\cr
-#     The predicted probability for class "classname". One column for each class. 
-#     If no probabilities are provided, the predicted class gets probability 1 and each other class 
-#     gets probability 0.}  
+#     The predicted probability for class "classname". One column for each class.
+#     If no probabilities are provided, the predicted class gets probability 1 and each other class
+#     gets probability 0.}
 
 reformatPredictions = function(pred, task) {
   iter = pred$iter
@@ -82,10 +82,10 @@ reformatPredictions = function(pred, task) {
   fold = iter %% folds
   fold[fold == 0] = folds
   rowid = pred$id
-  
+
   # Note: The columns rep, fold and row_id must be 0-based to be accepted by the server.
   new_pred = data.frame(rep = rep - 1, fold = fold - 1, row_id = rowid - 1, prediction = pred$response)
-  
+
   if (task$type == "Supervised Classification") {
     classes = levels(pred$response)
     probs = c()
@@ -96,13 +96,13 @@ reformatPredictions = function(pred, task) {
     } else {
       for (i in 1:length(classes)) {
         probs = cbind(probs, ifelse(pred$response == classes[i], 1, 0))
-      } 
+      }
     }
-    colnames(probs) = sprintf("confidence.%s", classes) 
-    
+    colnames(probs) = sprintf("confidence.%s", classes)
+
     new_pred = cbind(new_pred, probs)
   }
-  
-  colnames(new_pred)[1] = "repeat"  
+
+  colnames(new_pred)[1] = "repeat"
   return(new_pred)
 }
--- R/toMLR.R
+++ R/toMLR.R
@@ -1,9 +1,9 @@
 #' Convert an OpenML task to an MLR task object.
-#' 
-#' @param task [\code{\linkS4class{OpenMLTask}}]\cr 
+#'
+#' @param task [\code{\linkS4class{OpenMLTask}}]\cr
 #'   An OpenML task object. Required.
-#' @return [\code{\link[mlr]{SupervisedTask}}] 
-#' @export 
+#' @return [\code{\link[mlr]{SupervisedTask}}]
+#' @export
 
 toMLR = function(task) {
   checkArg(task, "OpenMLTask")
@@ -12,18 +12,18 @@ toMLR = function(task) {
   data.set.desc = task$data.desc
   data = task$data.desc$data.set
   target = task$target.features
-  
+
   #FIXME some data sets have empty factor levels, mlr does not like this
   # fix this for now by removing
   data = droplevels(data)
-  
+
   # FIXME: hack to convert bad feature names
   feature.ind = which(colnames(data) %nin% target)
   feature.names = colnames(data)[feature.ind]
-  feature.names = str_replace_all(feature.names, pattern=c("\\-"), replacement="_")  
-  feature.names = str_replace_all(feature.names, pattern=c("/"), replacement="_")  
+  feature.names = str_replace_all(feature.names, pattern=c("\\-"), replacement="_")
+  feature.names = str_replace_all(feature.names, pattern=c("/"), replacement="_")
   colnames(data)[feature.ind] = feature.names
-  
+
   estim.proc = task$estimation.procedure
   if (task.type == "Supervised Classification") {
     mlr.task = makeClassifTask(data = data, target = target)
@@ -43,16 +43,17 @@ createMLRResampleInstance = function(estim.proc, mlr.task) {
   n.folds = estim.proc$parameters[["number_folds"]]
   percentage = as.numeric(estim.proc$parameters[["percentage"]])
   data.splits = estim.proc$data.splits
-  #FIXME why is stratify TRUE here? does the server always prdoced stratified
-  # resampling for classif? check this
+  #FIXME: I think the server always prdoced stratified resampling for classif? we need to check this.
+  # if so, we need to set that property, but only after the split sets for mlr have been overwritten.
+  # otherwise in some case some mlr sanity check apparently gets triggered.
   stratify = (mlr.task$task.desc$type == "classif")
   # FIXME : more resampling
   if (type == "crossvalidation") {
     if (n.repeats == 1L)
-      mlr.rdesc = makeResampleDesc("CV", iters = n.folds, stratify = stratify)
-    else 
-      mlr.rdesc = makeResampleDesc("RepCV", reps = n.repeats, folds = n.folds, stratify = stratify)
-    mlr.rin = makeResampleInstance(mlr.rdesc, task = mlr.task)  
+      mlr.rdesc = makeResampleDesc("CV", iters = n.folds)
+    else
+      mlr.rdesc = makeResampleDesc("RepCV", reps = n.repeats, folds = n.folds)
+    mlr.rin = makeResampleInstance(mlr.rdesc, task = mlr.task)
   } else if (type == "holdout") {
     mlr.rdesc = makeResampleDesc("Holdout", split = 1 - percentage/100)
     mlr.rin = makeResampleInstance(mlr.rdesc, task = mlr.task)
@@ -64,7 +65,7 @@ createMLRResampleInstance = function(estim.proc, mlr.task) {
   for (r in 1:n.repeats) {
     for (f in 1:n.folds) {
       d = subset(data.splits, rep ==  r & data.splits$fold == f)
-      mlr.rin$train.inds[[iter]] = subset(d, type == "TRAIN")$rowid 
+      mlr.rin$train.inds[[iter]] = subset(d, type == "TRAIN")$rowid
       mlr.rin$test.inds[[iter]] = subset(d, type == "TEST")$rowid
       iter = iter + 1L
     }
@@ -78,30 +79,30 @@ createMLRResampleInstance = function(estim.proc, mlr.task) {
 createMLRMeasures = function(measures, type) {
   lapply(measures, function(m) {
     if(type == "Supervised Classification") {
-      switch(m, 
+      switch(m,
         mean_absolute_error = mmce,
         area_under_roc_curve = auc,
-        build_cpu_time = timetrain,     
+        build_cpu_time = timetrain,
         f_measure = f1,
         matthews_correlation_coefficient = mcc,
         precision = ppv,
         predictive_accuracy = acc,
         recall = tpr,
-        stopf("Unsupported evaluation measure: %s", m)     
+        stopf("Unsupported evaluation measure: %s", m)
       )
     } else {
-      switch(m, 
+      switch(m,
         root_mean_squared_error = rmse,
         mean_absolute_error = mae,
         area_under_roc_curve = auc,
-        build_cpu_time = timetrain,     
+        build_cpu_time = timetrain,
         f_measure = f1,
         matthews_correlation_coefficient = mcc,
         precision = ppv,
         predictive_accuracy = acc,
         recall = tpr,
-        stopf("Unsupported evaluation measure: %s", m)     
+        stopf("Unsupported evaluation measure: %s", m)
       )
-    } 
+    }
   })
 }
