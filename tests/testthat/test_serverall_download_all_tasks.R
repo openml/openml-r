30e9fbbc5ced79cfd06c2f4b357a848edfb6ffff
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-22 19:40:08 +0200

    clean up

--- tests/testthat/test_serverall_download_all_tasks.R
+++ tests/testthat/test_serverall_download_all_tasks.R
@@ -1,15 +1,15 @@
 context("download all tasks")
 
 test_that("download all tasks", {
-  # FIXME extend test to all task on openml server
+  # FIXME: extend test to all task on openml server
   # FIXME: problem in task 6, 8
   ids = 1:10
   for (i in ids) {
     print(i)
-    task = downloadOpenMLTask(id=i, show.info = TRUE, clean.up=TRUE)
-    tf = task$task.target.features
+    task = downloadOpenMLTask(id = i, show.info = TRUE, clean.up = TRUE)
+    tf = task$target.features
     expect_true(is.character(tf) && length(tf) == 1 && !is.na(tf))
-    ds = task$task.data.desc$data.set
+    ds = task$data.desc$data.set
     expect_true(is.data.frame(ds) && nrow(ds) > 1  && ncol(ds) > 1 )
     if (task$task.type == "Supervised Classification")
       lrn = makeLearner("classif.rpart")
