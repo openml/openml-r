2dcfcef24847b1af8a2518cf279cdc28e66e3fcd
Michel <michellang@gmail.com>
2014-10-20 13:13:25 +0200

    added travis.yml (fixes #28)

--- tests/testthat/helper_zzz.R
+++ tests/testthat/helper_zzz.R
@@ -1,1 +1,10 @@
 configureOpenML(show.info = FALSE)
+
+# remove this after testthat upload
+if (packageVersion("testthat") < "0.9.1.9000") {
+  skip_on_travis <- function() {
+    if (!identical(Sys.getenv("TRAVIS"), "true")) return()
+
+    skip("On Travis")
+  }
+}
--- tests/testthat/test_serverall_download_all_datasets.R
+++ tests/testthat/test_serverall_download_all_datasets.R
@@ -3,13 +3,15 @@ context("download all data sets")
 # we download "most" safe data sets, because large ones might cause runtime and mem problems currently
 
 test_that("download all data sets", {
+  skip_on_cran()
+  skip_on_travis()
   quals = getDataQualities()
   dsets = getOpenMLDatasetNames()
-  quals2 = subset(quals, dataset %in% dsets$name & NumberOfInstances <= 10000 & 
+  quals2 = subset(quals, dataset %in% dsets$name & NumberOfInstances <= 10000 &
     NumberOfFeatures <= 100 & dataset != "Zoo_test_jan")
 
   ids = quals2[, c("dataset", "version")]
-  
+
   for (i in 1:nrow(ids)) {
     if (ids[i, "version"] %in% dsets[dsets$name == ids[i, "dataset"], ]) {
       print(sprintf("%s, v. %i", ids[i, "dataset"], ids[i, "version"]))
