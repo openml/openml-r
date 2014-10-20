7f53263df43b9eaac207988762dc272f65b5c1fe
Michel <michellang@gmail.com>
2014-10-20 13:37:04 +0200

    fixed test for getTasksForDataSet

--- tests/testthat/test_base_getTasksForDataSet.R
+++ tests/testthat/test_base_getTasksForDataSet.R
@@ -2,7 +2,7 @@ context("getTasksForDataSet")
 
 test_that("getTasksForDataSet", {
   tasks.1 = getTasksForDataSet(1)
-  expect_is(tasks_did1, "integer")
+  expect_is(tasks.1, "integer")
   tasks.2 = getTasksForDataSet(2)
   tasks.1and2 = getTasksForDataSet(c(1, 2))
   expect_equal(length(tasks.1and2), length(tasks.1) + length(tasks.2))
