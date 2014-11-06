7559b855d1f32deb8c942b6bffa06babdf7617e5
Dominik Kirchhoff <dominik.kirchhoff@tu-dortmund.de>
2014-11-06 17:48:33 +0100

    bug

--- tests/testthat/test_base_authenticateUser.R
+++ tests/testthat/test_base_authenticateUser.R
@@ -2,7 +2,7 @@ context("authenticateUser")
 
 test_that("authenticateUser", {
   hash = authenticateUser("openml.rteam@gmail.com", "testpassword")
-  expect_true(testString(hash))
+  expect_true(is.character(hash) && length(hash) == 1)
   
   expect_error(authenticateUser("xxx", "xxx"))
 })
