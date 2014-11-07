3ed9b8e3552c474c99e09ed7ed7d00e1763e0464
Michel <michellang@gmail.com>
2014-11-07 14:58:15 +0100

    renamed more files

--- R/download.R
+++ R/download.R
@@ -1,8 +1,44 @@
+getAPIURL = function(fname, secure = NULL, ...) {
+  url = sprintf("%s/?f=%s",
+    getServerURL(secure),
+    fname)
+  args = list(...)
+  if (length(args) > 0L) {
+    args = collapse(paste(names(args), args, sep = "="), sep = "&")
+    url = paste(url, args, sep = "&")
+  }
+  return(url)
+}
+
+getServerURL = function(secure = NULL) {
+  url = getOMLConfig()$server
+  if (!is.null(secure)) {
+    pr = ifelse(secure, "https://", "http://")
+    split = unlist(str_split(url, pattern = "://"))
+    if (length(split) == 1L) {
+      url = paste0(pr, split)
+    } else if (length(split) == 2L) {
+      split[1L] = pr
+      url = collapse(split, sep = "")
+    } else {
+      stop("The server URL in your config file looks strange. Try 'http://www.openml.org'.")
+    }
+  }
+  last.char = str_sub(url, str_length(url), str_length(url))
+  api = ifelse(last.char == "/", "api", "/api")
+  url = paste0(url, api)
+  return(url)
+}
+
 # download xml from a url / api call to file
 # if the file is NULL, we just retrieve the content, which is returned in any case
-downloadXML = function(url, file, verbosity = NULL) {
+downloadXML = function(url, file, verbosity = NULL, ...) {
   showInfo(verbosity, "Downloading '%s' to '%s'", url, ifelse(is.null(file), "<mem>", file))
-  content = getURL(url)
+  ddd = list(...)
+  content = if (length(ddd) > 0L)
+    postForm(url, ...)
+  else
+    getURL(url)
   if (!is.null(file)) {
     con = file(file, open = "w")
     on.exit(close(con))
@@ -17,15 +53,4 @@ downloadARFF = function(url, file, verbosity = NULL) {
   #FIXME: get real verbosity level here >= info
   download.file(url, file, mode = "w", quiet = TRUE)
   read.arff(file)
-}
-
-postFormOML = function(url, file, verbosity = NULL, ...) {
-  showInfo(verbosity, "Downloading '%s' to '%s'", url, ifelse(is.null(file), "<mem>", file))
-  content = postForm(url, ...)
-  if (!is.null(file)) {
-    con = file(file, open = "w")
-    on.exit(close(con))
-    writeLines(content, con = con)
-  }
-  return(content)
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,17 @@
 #' @import BBmisc
+#' @import checkmate
 #' @import digest
 #' @import RCurl
-#' @import rjson
 #' @import stringr
 #' @import XML
 #' @import RWeka
 #' @import mlr
-#' @import checkmate
 #' @importFrom data.table rbindlist
 #' @importFrom stats setNames
 NULL
 
 .OpenML.config = new.env(parent = emptyenv())
-class(.OpenML.config) = c("OMLConfig", class(.OpenML.config))
+class(.OpenML.config) = "OMLConfig"
 
 .onAttach = function(libname, pkgname) {
   readConfigAndAssign()
