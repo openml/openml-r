a68a121ebd9f108ca2fd9ceb4f8ee5d0d4c4d458
Bernd Bischl <bernd_bischl@gmx.net>
2015-02-20 18:40:08 +0100

    listOMLDataSets: include dset name

--- R/listOMLDataSets.R
+++ R/listOMLDataSets.R
@@ -9,7 +9,7 @@
 #' @return [\code{data.frame}].
 #' @family list
 #' @export
-listOMLDataSets = function(session.hash = getSessionHash(), 
+listOMLDataSets = function(session.hash = getSessionHash(),
     verbosity = NULL, status = "active") {
   url = getAPIURL("openml.data")
   content = downloadXML(url, NULL, verbosity, session_hash = session.hash)
@@ -17,18 +17,22 @@ listOMLDataSets = function(session.hash = getSessionHash(),
   status.levels = c("active", "deactivated", "in_preparation")
   assertSubset(status, status.levels)
   assertString(session.hash)
-  
+
   # get list of blocks for data sets
   blocks = xmlChildren(xmlChildren(xml)[[1L]])
   df = as.data.frame(rbindlist(lapply(blocks, function(node) {
     children = xmlChildren(node)
     is.quality = names(children) == "quality"
-    info = list(did = as.integer(xmlValue(children[["did"]])), status = xmlValue(children[["status"]]))
+    info = list(
+      did = as.integer(xmlValue(children[["did"]])),
+      status = xmlValue(children[["status"]]),
+      name = xmlValue(children[["name"]])
+    )
     qualities = convertNodeSetToList(children[is.quality], fun = as.integer)
     c(info, qualities)
   }), fill = TRUE))
   df$status = factor(df$status, levels = status.levels)
-  
+
   # subset status level
   ret = droplevels(df[df$status%in%status, ])
   row.names(ret) = NULL
--- tests/testthat/test_base_listOMLDataSets.R
+++ tests/testthat/test_base_listOMLDataSets.R
@@ -3,8 +3,10 @@ context("listOMLDataSets")
 test_that("listOMLDataSets", {
   dsl = listOMLDataSets(session.hash)
   expect_is(dsl, "data.frame")
-  expect_true(nrow(dsl) > 100L && ncol(dsl) == 9L)
-  expect_true(setequal(names(dsl), c("did", "status", "NumberOfClasses", "NumberOfFeatures", "NumberOfInstances", "NumberOfInstancesWithMissingValues", "NumberOfMissingValues", "NumberOfNumericFeatures", "NumberOfSymbolicFeatures")))
-  inds = which(names(dsl) != "status")
+  expect_true(nrow(dsl) > 100L && ncol(dsl) == 10L)
+  expect_true(setequal(names(dsl), c("did", "status", "name", "NumberOfClasses", "NumberOfFeatures",
+    "NumberOfInstances", "NumberOfInstancesWithMissingValues", "NumberOfMissingValues",
+    "NumberOfNumericFeatures", "NumberOfSymbolicFeatures")))
+  inds = which(names(dsl) %nin% c("status", "name"))
   expect_true(all(apply(dsl[, inds], 2, is.integer)))
 })
