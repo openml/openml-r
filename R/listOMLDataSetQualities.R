1defe741214d141d4c1a36ed0c6be79e11a31442
giuseppec <giuseppe.casalicchio@gmail.com>
2015-04-27 09:02:11 +0200

    extend description

--- R/listOMLDataSetQualities.R
+++ R/listOMLDataSetQualities.R
@@ -3,25 +3,38 @@
 #' @description
 #' The returned \code{data.frame} contains data set quality \dQuote{name} and value \dQuote{value}.
 #'
+#' @param data.id [\code{integer(1)}]\cr
+#'   The data set ID
 #' @template arg_hash
 #' @template arg_verbosity
+#' @param name [\code{character}]\cr
+#'   Retruns only the data qualities from \dQuote{name} (see also \link{\link{listOMLDataSetQualitiesList}}).
+#'   Default is \code{NULL} and uses all available data qualities.
 #' @return [\code{data.frame}].
 #' @family list
 #' @export
 #'
 listOMLDataSetQualities = function(data.id, session.hash = getSessionHash(),
-    verbosity = NULL) {
+    verbosity = NULL, name = NULL) {
   assertString(session.hash)
+#   qualities = listOMLDataSetQualitiesList()$name
+#   if(is.null(name)) name = qualities
+#   assertSubset(name, qualities)
+  
   url = getAPIURL("openml.data.qualities", data_id = data.id)
-  content = downloadXML(url, NULL, verbosity = verbosity, session_hash = session.hash)
+  content = downloadXML(url, NULL, verbosity = verbosity, session_hash = session.hash, post = FALSE)
   xml = parseXMLResponse(content, "Getting data set qualities", "data_qualities", as.text = TRUE)
 
   blocks = xmlChildren(xmlChildren(xml)[[1L]])
-  as.data.frame(rbindlist(lapply(blocks, function(node) {
+  ret = as.data.frame(rbindlist(lapply(blocks, function(node) {
     children = xmlChildren(node)
     list(
       name = xmlValue(children[["name"]]),
       value = as.numeric(xmlValue(children[["value"]]))
     )
   }), fill = TRUE))
+  
+  if(is.null(name)) return(ret) 
+  if(any(ret$name%in%name)) return(ret[ret$name%in%name, ]) else 
+    stop("Data quality in 'name' not found.")
 }
