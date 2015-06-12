0128f1098d04f0582120d95c0a666cc83e94b6e4
Michel <michellang@gmail.com>
2015-06-12 11:54:29 +0200

    fixed dataset qualities test

--- tests/testthat/test_base_listOMLDataSetQualities.R
+++ tests/testthat/test_base_listOMLDataSetQualities.R
@@ -1,17 +1,12 @@
 context("listOMLDataSetQualities")
 
 test_that("listOMLDataSetQualities", {
-  qualities.ds1 = listOMLDataSetQualities(1L, session.hash)
-  expect_is(qualities.ds1, "data.frame")
-  expect_true(all(dim(qualities.ds1) == c(9, 2)))
-  expect_true(all(apply(qualities.ds1[1], 2, is.character)))
-  expect_true(all(apply(qualities.ds1[2], 2, is.numeric)))
-  expect_true(setequal(names(qualities.ds1), c("name", "value")))
-
-  qualities.ds2 = listOMLDataSetQualities(2L, session.hash)
-  expect_is(qualities.ds2, "data.frame")
-  expect_true(all(dim(qualities.ds2) == c(64, 2)))
-  expect_true(all(apply(qualities.ds2[1], 2, is.character)))
-  expect_true(all(apply(qualities.ds2[2], 2, is.numeric)))
-  expect_true(setequal(names(qualities.ds2), c("name", "value")))
+  for (id in 1:2) {
+    qual = listOMLDataSetQualities(id, session.hash)
+    expect_is(qual, "data.frame")
+    expect_true(all(c("ClassCount", "ClassEntropy", "NoiseToSignalRatio") %in% qual$name))
+    expect_true(is.character(qual[, 1L]))
+    expect_true(is.numeric(qual[, 2L]))
+    expect_equal(names(qual), c("name", "value"))
+  }
 })
