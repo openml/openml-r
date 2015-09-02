62acfdb8dc1690aca211a92e335466deb3bd3601
Bernd Bischl <bernd_bischl@gmx.net>
2015-09-03 00:32:01 +0200

    new api tests

--- tests/testthat/test_base_listOMLTasks.R
+++ tests/testthat/test_base_listOMLTasks.R
@@ -1,17 +1,7 @@
 context("listOMLTasks")
 
 test_that("listOMLTasks", {
-  tasks.cl = listOMLTasks(1L, session.hash)
-  expect_is(tasks.cl, "data.frame")
+  tasks = listOMLTasks()
+  expect_is(tasks, "data.frame")
   expect_true(nrow(tasks.cl) > 5L)
-  expect_true(all(tasks.cl$task_type == "Supervised Classification"))
-
-  tasks.reg = listOMLTasks(2L, session.hash)
-  expect_is(tasks.reg, "data.frame")
-  expect_true(nrow(tasks.cl) > 1L)
-  expect_true(all(tasks.reg$task_type == "Supervised Regression"))
-
-  expect_true(length(intersect(tasks.cl$task_id, tasks.reg$task_id)) == 0)
-
-  expect_equal(dim(listOMLTasks(0L, session.hash)), c(0, 0))
 })
