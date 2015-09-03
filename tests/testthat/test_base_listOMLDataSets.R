72e09c8b425356d240e250bb487e731841a2f8ca
casalicchio <giuseppe.casalicchio@gmail.com>
2015-09-03 09:32:10 +0200

    fix tests to work with the API v1

--- tests/testthat/test_base_listOMLDataSets.R
+++ tests/testthat/test_base_listOMLDataSets.R
@@ -7,6 +7,6 @@ test_that("listOMLDataSets", {
   expect_true(setequal(names(dsl), c("did", "status", "name", "NumberOfClasses", "NumberOfFeatures",
     "NumberOfInstances", "NumberOfInstancesWithMissingValues", "NumberOfMissingValues",
     "NumberOfNumericFeatures", "NumberOfSymbolicFeatures")))
-  inds = which(names(dsl) %nin% c("status", "name"))
-  expect_true(all(apply(dsl[, inds], 2, is.integer)))
+  inds = which(names(dsl) %in% c("status", "name"))
+  expect_true(all(apply(dsl[, inds], 2, is.character)))
 })
--- tests/testthat/test_base_listOMLRunResults.R
+++ tests/testthat/test_base_listOMLRunResults.R
@@ -1,7 +1,7 @@
 context("listOMLRunResults")
 
 test_that("listOMLRunResults", {
-  rr = listOMLRunResults(task.id = 1L, session.hash)
+  rr = listOMLRunResults(task.id = 1L)
   expect_is(rr, "data.frame")
   expect_true(nrow(rr) > 100L && ncol(rr) >= 20L)
   expect_true(all(c("run.id", "setup.id", "implementation.id", "implementation", "task.id", "task.type.id", "estim.proc") %in% names(rr)))
