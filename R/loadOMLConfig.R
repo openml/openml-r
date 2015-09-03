b287462e0337a6d2a395812dc4c1b51946af29d5
Jakob Bossek <info@jakobbossek.de>
2015-09-03 16:13:44 +0200

    add saveOMLConfig

---
+++ R/loadOMLConfig.R
@@ -0,0 +1,16 @@
+#' Loads a config file from disk to mem
+#'
+#' @param path [\code{character(1)}]\cr
+#'   full path location of the config file to be loaded
+#' @return \code{list} of current configuration variables with class \dQuote{OMLConfig}.
+#' @family config
+#' @export
+loadOMLConf = function(path = "~/.openml/config") {
+  assertFile(path)
+  # read and assign config file
+  conf = readConfigFile(path.expand(path))
+  assignConfig(conf)
+  # create cache dir from new config file
+  createCacheSubDirs(verbosity = FALSE)
+  invisible(conf)
+}
