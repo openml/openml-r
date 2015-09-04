045eabfa680b7e0b32bcf068d2ce66e5054e9256
Jakob Bossek <info@jakobbossek.de>
2015-09-04 10:20:41 +0200

    update saveOMLConfig

--- R/saveOMLConfig.R
+++ R/saveOMLConfig.R
@@ -1,26 +1,41 @@
 #' @title Saves a list of OpenML configuration settings to file
 #'
-#' @param conf [\code{list}]\cr
-#'   Named list of configuration values.
-#' @param ... [any]\cr
-#'   Another possibility to pass configuration options.
+#' @param server [\code{character(1)}]\cr
+#'   URL of the XML API endpoint.
+#' @param verbosity [\code{integer(1)}]\cr
+#'   Verbosity level. Possible values are 0 (no output), 1 (), 2() and 3 ().
+#'   Default is 1.
+#' @param apikey [\code{character(1)}]\cr
+#'   Your OpenML API key. Log in to OpenML, move to your profile to get it.
+#' @param cachedir [\code{character(1)}]\cr
+#'   Path the the cache directory.
+#' @param arff.reader [\code{character(1)}]\cr
+#'   Name of the package which should be used to parse arff files. Possible are
+#'   \dQuote{RWeka}, which is the default and \dQuote{farff}.
 #' @param path [\code{character(1)}]\cr
 #'   Path to OpenML config file. Default is \dQuote{~/.openml/config}.
 #' @param overwrite [\code{logical(1)}]\cr
 #'   Should an existing file be overwritten? Default is \code{FALSE}.
 #' @family config
 #' @export
-saveOMLConfig = function(conf = list(), ..., path = "~/.openml/config", overwrite = FALSE) {
+saveOMLConfig = function(server = NULL, verbosity = NULL,
+  apikey = NA, cachedir = NULL, arff.reader = "RWeka", path = "~/.openml/config",
+  overwrite = FALSE) {
   assertFlag(overwrite)
-  conf = insert(conf, list(...))
-  checkConfig(conf)
 
   if (file.exists(path) && !overwrite) {
     stopf("Configuration file %s already exists. Set override to TRUE to force overwriting.", path)
   }
 
+  if (!file.exists(dirname(path))) {
+    dir.create(dirname(path), showWarnings = TRUE, recursive = TRUE)
+  }
+
+  conf = setOMLConfig(server = server, verbosity = verbosity,
+    apikey = apikey, cachedir = cachedir,
+    arff.reader = arff.reader)
+  conf = as.list(conf)
   lines = paste(names(conf), conf, sep = "=")
-  print(lines)
   con = file(path, "w")
   on.exit(close(con))
   writeLines(lines, con = con)
