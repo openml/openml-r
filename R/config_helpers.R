e2f1ea79a26b5534c8864ce2dfb9a6aae4b0e906
giuseppec <giuseppe.casalicchio@gmail.com>
2015-09-09 14:35:39 +0200

    remove redundant function, see getOMLConfig.R file

--- R/config_helpers.R
+++ R/config_helpers.R
@@ -53,12 +53,3 @@ printableConfig = function(conf) {
 print.OMLConfig = function(x, ...) {
   cat(printableConfig(x))
 }
-
-#' @title Returns a list of OpenML configuration settings.
-#'
-#' @return \code{list} of current configuration variables with class \dQuote{OMLConfig}.
-#' @family config
-#' @export
-getOMLConfig = function() {
-  return(.OpenML.config)
-}
