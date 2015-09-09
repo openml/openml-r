84869cd56b133f1927125c6d8579e0a71d0910b8
giuseppec <giuseppe.casalicchio@gmail.com>
2015-09-09 11:08:28 +0200

    fix #93: speedup by factor 1.8

--- R/listOMLTasks.R
+++ R/listOMLTasks.R
@@ -9,50 +9,50 @@
 #' @return [\code{data.frame}].
 #' @export
 listOMLTasks = function(verbosity = NULL, status = "active") {
-  #type = asInt(type)
   status.levels = c("active", "deactivated", "in_preparation")
   assertSubset(status, status.levels)
-
+  
   content = try(doAPICall(api.call = "task/list", file = NULL, verbosity = verbosity, method = "GET"))
-
-  if (is.error(content))
-    return(data.frame())
-
-  xml = parseXMLResponse(content, "Getting task list", "tasks", as.text = TRUE)
-
-  blocks = xmlChildren(xmlChildren(xml)[[1L]])
-  df = rbindlist(lapply(blocks, function(node) {
-    children = xmlChildren(node)
-    is.quality = names(children) == "quality"
-    is.input = names(children) == "input"
-    is.tag = names(children) == "tag"
-    inputs = children[is.input]
-    tags = children[is.tag]
-    tags = lapply(tags, xmlValue)
-    tags = collapse(tags, sep = ", ")
-    names(inputs) = vcapply(inputs, function(x) xmlAttrs(x)[["name"]])
-
-    info = list(
-        task_id = as.integer(xmlValue(children[["task_id"]])),
-        task_type = xmlValue(children[["task_type"]]),
-        did = as.integer(xmlValue(children[["did"]])),
-        status = xmlValue(children[["status"]]),
-        name = xmlValue(children[["name"]]),
-        tags = tags,
-        estimation_procedure = as.integer(xmlValue(inputs[["estimation_procedure"]])),
-        evaluation_measures = collapse(as.character(xmlValue(inputs[["evaluation_measures"]])))
-    )
-    qualities = convertNodeSetToList(children[is.quality], as.integer)
-    c(info, qualities)
-  }), fill = TRUE)
-
-  estproc = listOMLEstimationProcedures()
+  
+  if (is.error(content)) return(data.frame())
+  
+  d = xmlRoot(xmlParse(content))
+  
+  # get values from each XML string
+  string.list = xmlSApply(d, getChildrenStrings)
+  # get indices where string.status is included in status
+  string.ind = which(vcapply(string.list, function(X) X["status"])%in%status)
+  
+  # subset with respect to 'status' (speedup)
+  string.list = string.list[string.ind]
+  child.list = sapply(d[string.ind], xmlChildren)
+  
+  info = lapply(1:length(string.list), function(X) {
+    strings = string.list[[X]]
+    child = child.list[[X]]
+    # get index of input and quality childrens
+    ind = names(child)%in%c("input", "quality")
+    # FIXME: not looking up names with xmlAttrs would speedup the code, can we use fixed names here?
+    # get the name attributes of them
+    names = vcapply(child[ind], xmlAttrs, "name")
+    # replace the names of the children-string with the names attribute
+    names(strings)[ind] = names
+    # get the tag indices and paste them together as single column
+    tag.ind = names(strings) == "tag"
+    strings = c(strings[!tag.ind], "tags" = collapse(strings[tag.ind], sep = ", "))
+    out.vars = c("task_id", "task_type", "did", "status", "name", "tags", 
+                 "estimation_procedure", "evaluation_measures", names[names(names)%in%"quality"])
+    return(as.list(strings[out.vars]))
+  } )
+  li = as.data.frame(rbindlist(info, fill = TRUE))
+  li = li[, !is.na(colnames(li))]
+  int.vars = setdiff(colnames(li), c("task_type", "status", "name", "tags", "evaluation_measures"))
+  li[, int.vars] = lapply(int.vars, function(x) as.integer(li[, x])) #sapply(li[,int.vars], as.integer)
+  
+  estproc = listOMLEstimationProcedures(verbosity = FALSE)
   row.names(estproc) = estproc$est.id
-  df$estimation_procedure = estproc[as.character(df$estimation_procedure), "name"]
-  df$status = factor(df$status, levels = status.levels)
-
-  # subset status level
-  ret = droplevels(as.data.frame(df)[df$status %in% status, ])
-  row.names(ret) = NULL
-  return(ret)
+  li$estimation_procedure = droplevels(estproc[as.character(li$estimation_procedure), "name"])
+  li$status = as.factor(li$status)
+  
+  return(li)
 }
