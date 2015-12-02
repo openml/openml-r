7f12afd3080caff537a1eda655064c1e963a6c5c
giuseppec <giuseppe.casalicchio@gmail.com>
2015-12-02 12:02:56 +0100

    add short unit test for saveOMLConfig (Issue #81)

---
+++ tests/testthat/test_base_saveOMLConfig.R
@@ -0,0 +1,28 @@
+context("saveOMLConfig")
+
+test_that("saveOMLConfig", {
+  # load config file
+  curr.conf = as.list(getOMLConfig()) #as.list(loadOMLConfig())
+  
+  # Overwrite config file only if overwrite = TRUE is expicitly set
+  expect_error(saveOMLConfig(arff.reader = "RWeka"), "Set override to TRUE to force overwriting.")
+  
+  # change arff.reader and check if changes were stored in config file
+  saveOMLConfig(arff.reader = "RWeka", overwrite = TRUE)
+  conf.RWeka = as.list(loadOMLConfig())
+  expect_true(conf.RWeka$arff.reader == "RWeka")
+  expect_equal(conf.RWeka, as.list(getOMLConfig()))
+  saveOMLConfig(arff.reader = "farff", overwrite = TRUE)
+  conf.farff = as.list(loadOMLConfig())
+  expect_true(conf.farff$arff.reader == "farff")
+  expect_equal(conf.farff, as.list(getOMLConfig()))
+  
+  # write config in different file
+  conf.file = tempfile()
+  saveOMLConfig(arff.reader = "RWeka", verbosity = 2, path = conf.file)
+  conf = as.list(loadOMLConfig(path = conf.file))
+  expect_true(conf$arff.reader == "RWeka" && conf$verbosity == 2)
+  
+  # set previous configs
+  do.call("setOMLConfig", curr.conf)
+})
