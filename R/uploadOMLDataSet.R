ac16186964fac9081a6ca776d0d8121aa6fd05f2
casalicchio <giuseppe.casalicchio@gmail.com>
2016-03-15 16:36:32 +0100

    fix #150: allow to add data set descriptions

--- R/uploadOMLDataSet.R
+++ R/uploadOMLDataSet.R
@@ -8,21 +8,22 @@
 #'
 #' @param x [\code{\link[mlr]{Task}}|[\code{\link{OMLDataSet}}]\cr
 #'   Contains the dataset that should be uploaded.
+#' @template arg_description
 #' @template arg_verbosity
 #' @return [\code{invisible(numeric(1))}].
 #'   The id of the data (\code{did}).
 #' @family uploading functions
 #' @family dataset related functions
 #' @export
-uploadOMLDataSet = function(x, verbosity = NULL) {
+uploadOMLDataSet = function(x, description = NULL, verbosity = NULL) {
   UseMethod("uploadOMLDataSet")
 }
 
 #' @export
-uploadOMLDataSet.OMLDataSet = function(x, verbosity = NULL) {
-  description = tempfile()
-  on.exit(unlink(description))
-  writeOMLDataSetXML(x$desc, description)
+uploadOMLDataSet.OMLDataSet = function(x, description = NULL, verbosity = NULL) {
+  desc.file = tempfile()
+  on.exit(unlink(desc.file))
+  writeOMLDataSetXML(x$desc, desc.file)
 
   output = tempfile()
   on.exit(unlink(output), add = TRUE)
@@ -34,7 +35,7 @@ uploadOMLDataSet.OMLDataSet = function(x, verbosity = NULL) {
   showInfo(verbosity, "Uploading data set to server.")
 
   response = doAPICall(api.call = "data", method = "POST", file = NULL, verbosity = verbosity,
-    post.args = list(description = upload_file(path = description),
+    post.args = list(description = upload_file(path = desc.file),
                      dataset = upload_file(path = output)))
   doc = parseXMLResponse(response, "Uploading dataset", c("upload_data_set", "response"), as.text = TRUE)
   did = xmlOValI(doc, "/oml:upload_data_set/oml:id")
@@ -44,24 +45,46 @@ uploadOMLDataSet.OMLDataSet = function(x, verbosity = NULL) {
 }
 
 #' @export
-uploadOMLDataSet.Task = function(x, verbosity = NULL) {
-  x = createOMLDataSetFromMlrTask(x)
+uploadOMLDataSet.Task = function(x, description = NULL, verbosity = NULL) {
+  x = convertMlrTaskToOMLDataSet(x, description = description)
   uploadOMLDataSet.OMLDataSet(x)
 }
 
-createOMLDataSetFromMlrTask = function(task){
-  desc = makeOMLDataSetDescription(id = 1L,
-    name = task$task.desc$id,
-    version = "1",
-    description = task$task.desc$id,
-    format = "ARFF",
-    upload.date = as.POSIXct(Sys.time()),
-    default.target.attribute = task$task.desc$target,
-    status = "active"
-  )
-
+#' @title Converts a mlr task to an OpenML data set.
+#'
+#' @description
+#' Converts a \code{\link[mlr]{Task}} to an \code{\link{OMLDataSet}}.
+#'
+#' @param task [\code{\link[mlr]{Task}}]\cr
+#'   A mlr task.   
+#' @template arg_description
+#' 
+#' @return [\code{\link{OMLDataSet}}].
+#' @family dataset related functions
+#' @export
+convertMlrTaskToOMLDataSet = function(task, description = NULL, ...){
+  assert(checkClass(description, "character"), checkClass(description, "OMLDataSetDescription"), checkNull(description))
+  assertClass(task, "Task")
+  
+  if (is.null(description))
+    description = as.character(task$task.desc$id)
+  
+  if (isTRUE(checkClass(description, "OMLDataSetDescription"))) {
+    desc = description
+  } else {
+    desc = makeOMLDataSetDescription(
+      name = task$task.desc$id,
+      version = "1",
+      description = description,
+      format = "ARFF",
+      upload.date = as.POSIXct(Sys.time()),
+      default.target.attribute = task$task.desc$target,
+      status = "active"
+    )
+  }
+  
   cns = colnames(mlr::getTaskData(task))
-
+  
   oml.data = makeOMLDataSet(desc = desc,
     data = mlr::getTaskData(task),
     colnames.old = cns,
---
+++ tests/testthat/test_base_convertMlrTaskToOMLDataSet.R
@@ -0,0 +1,9 @@
+context("convertMlrTaskToOMLDataSet")
+
+test_that("convertMlrTaskToOMLDataSet", {
+  oml.iris = convertMlrTaskToOMLDataSet(iris.task)
+  expect_equal(oml.iris$colnames.old, oml.iris$colnames.new)
+  expect_equal(oml.iris$colnames.old, colnames(oml.iris$data))
+  expect_equal(oml.iris$target.features, getTaskTargetNames(iris.task))
+  expect_equal(oml.iris$data, getTaskData(iris.task))
+})
