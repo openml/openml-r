0fb57390f5bdc7cca0a32ec8969ef7275e05df76
Michel <michellang@gmail.com>
2016-03-17 10:53:58 +0100

    more helpers for tests

--- tests/testthat/helper_with.r
+++ tests/testthat/helper_with.r
@@ -1,13 +1,22 @@
 with_test_cache = function(expr, envir = parent.frame()) {
   prev = as.list(getOMLConfig())
   on.exit(do.call(setOMLConfig, prev))
-  setOMLConfig(cachedir = file.path(find.package("OpenML"), "tests", "cache"))
+  setOMLConfig(cachedir = file.path(find.package("OpenML"), "tests", "cache"), apikey = strrep("0", 16))
   eval(expr, envir = envir)
 }
 
-with_no_cache = function(expr, envir = parent.frame()) {
+with_empty_cache = function(expr, envir = parent.frame()) {
   prev = as.list(getOMLConfig())
   on.exit(do.call(setOMLConfig, prev))
-  setOMLConfig(cachedir = tempfile())
+  dir = tempfile()
+  dir.create(dir, recursive = TRUE)
+  on.exit(unlink(dir, recursive = TRUE), add = TRUE)
+  setOMLConfig(cachedir = dir)
   eval(expr, envir = envir)
+}
+
+regen_test_cache = function() {
+  with_test_cache({
+    getOMLTask(task.id = 59)
+  })
 }
