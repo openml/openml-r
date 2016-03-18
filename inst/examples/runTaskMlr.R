519dd0885e34b6f38a229edd2ec587a6814a03b8
Pascal Kerschke <pascal.kerschke@uni-muenster.de>
2016-03-18 11:47:51 +0100

    trying to see what's going wrong in the examples

--- inst/examples/runTaskMlr.R
+++ inst/examples/runTaskMlr.R
@@ -1,5 +1,11 @@
 library(mlr)
 ## run a single flow (learner) on a single task
+conf = getOMLConfig()
+print(conf$apikey)
+setOMLConfig(apikey = "c1994bdb7ecb3c6f3c8f3b35f4b47f1f")
+conf = getOMLConfig()
+print(conf$apikey)
+
 task = getOMLTask(57)
 lrn = makeLearner("classif.rpart")
 res = runTaskMlr(task, lrn)
