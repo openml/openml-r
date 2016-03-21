8005e279641834e6d88779a636fe802643dee281
giuseppec <giuseppe.casalicchio@gmail.com>
2016-03-21 16:18:28 +0100

    try to fix travis

--- tests/testthat/helper_with.r
+++ tests/testthat/helper_with.r
@@ -1,7 +1,7 @@
 with_test_cache = function(expr, envir = parent.frame()) {
   prev = as.list(getOMLConfig())
   on.exit(do.call(setOMLConfig, prev))
-  setOMLConfig(cachedir = file.path(find.package("OpenML"), "tests", "cache"), apikey = strrep("0", 32))
+  setOMLConfig(cachedir = file.path(find.package("OpenML"), "tests", "cache"))
   eval(expr, envir = envir)
 }
 
