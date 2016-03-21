686389275090bad766063eb7638dcae4da4100f1
giuseppec <giuseppe.casalicchio@gmail.com>
2016-03-21 16:18:28 +0100

    try to fix travis

--- tests/testthat/test_server_deleteOMLObject.R
+++ tests/testthat/test_server_deleteOMLObject.R
@@ -1,8 +1,6 @@
 context("deleteOMLObject")
 
 test_that("deleteOMLObject", {
-  run = getOMLRun(1)
-  run.id = uploadOMLRun(run)
   expect_error(deleteOMLObject(id = 1, object = "run"), "Run is not owned by you")
   expect_error(deleteOMLObject(id = 123456789, object = "run"), "Run does not exists")
 })
