9c3492746c3aa2823f91897fb39e89cd730b4c80
giuseppec <giuseppe.casalicchio@gmail.com>
2016-05-20 22:51:21 +0200

    add files to test cache

--- tests/testthat/test_local_runTaskFlow.R
+++ tests/testthat/test_local_runTaskFlow.R
@@ -1,48 +1,38 @@
 context("runTaskFlow")
 
 test_that("runTaskFlow", {
-  with_empty_cache({
-    with_write_access({
-      checkRun = function(res) {
-        expect_is(res, "OMLMlrRun")
-        expect_equal(length(res), 3L)
-        expect_is(res$run$predictions, "data.frame")
-        expect_is(res$bmr, "BenchmarkResult")
-        expect_is(res$flow, "OMLFlow")
-      }
-      
-      task = getOMLTask(3736) #3
-      #task$input$evaluation.measures = "area_under_roc_curve"
-      
-      # # use a learner without parameter values
-      # lrn = makeLearner("classif.logreg")
-      # res = runTaskMlr(task, lrn)
-      # 
-      # # upload run, download it again and try to reproduce it locally
-      # run.id = uploadOMLRun(res)
-      # run = getOMLRun(run.id)
-      # flow = getOMLFlow(run$flow.id)
-      # res2 = runTaskFlow(task, flow, par.list = getOMLRunParList(run), seed = getOMLSeedParList(run))
-      # checkRun(res2)
-      # expect_equal(res$bmr$results[[1]][[1]]$measures.test$acc,
-      #   res2$bmr$results[[1]][[1]]$measures.test$acc)
-      
-      # do the same again and use a learner with parameter values
-      lrn = makeLearner("classif.randomForest", mtry = 2, ntree = 5)
-      res = runTaskMlr(task, lrn)
-      run.id = uploadOMLRun(res)
-      run = getOMLRun(run.id)
-      flow = getOMLFlow(run$flow.id)
-      res2 = runTaskFlow(task, flow, par.list = getOMLRunParList(run), seed = getOMLSeedParList(run))
-      checkRun(res2)
-      expect_equal(res$bmr$results[[1]][[1]]$measures.test$acc,
-        res2$bmr$results[[1]][[1]]$measures.test$acc)
-      
-      # check if changing the seed yields different results (for RF it should)
-      res3 = runTaskFlow(task, flow, par.list = getOMLRunParList(run), seed = 123456)
-      checkRun(res3)
-      expect_false(isTRUE(all.equal(res$bmr$results[[1]][[1]]$measures.test$acc, 
-        res3$bmr$results[[1]][[1]]$measures.test$acc)))
-    })
+  with_test_cache({
+    checkRun = function(res) {
+      expect_is(res, "OMLMlrRun")
+      expect_equal(length(res), 3L)
+      expect_is(res$run$predictions, "data.frame")
+      expect_is(res$bmr, "BenchmarkResult")
+      expect_is(res$flow, "OMLFlow")
+    }
+    getOMLRunEvaluations = function(run, measures, aggr = TRUE) {
+      evals = run$output.data$evaluations
+      evals = evals[evals$name %in% measures, ]
+      if (aggr) return(evals[is.na(evals$fold) & is.na(evals[,"repeat"]), ]) else
+        return(evals[!is.na(evals$fold) & !is.na(evals[,"repeat"]), ])
+    }
+    
+    task = getOMLTask(59) #3
+    
+    # do the same again and use a learner with parameter values
+    lrn = makeFilterWrapper(makeLearner("classif.randomForest", mtry = 2, ntree = 5), fw.method = "variance", fw.perc = 0.5)
+    run = getOMLRun(572944)
+    flow = getOMLFlow(run$flow.id)
+    res2 = runTaskFlow(task, flow, par.list = getOMLRunParList(run), seed = getOMLSeedParList(run))
+    checkRun(res2)
+    # compare with acc value computed from run
+    acc = getOMLRunEvaluations(run, "predictive_accuracy", aggr = FALSE)$value
+    acc2 = getBMRPerformances(res2$bmr)[[1]][[1]][["acc"]]
+    expect_equal(round(acc, 5), round(acc2, 5))
+    
+    # check if changing the seed yields different results (for RF it should)
+    res3 = runTaskFlow(task, flow, par.list = getOMLRunParList(run), seed = 123456)
+    checkRun(res3)
+    acc3 = getBMRPerformances(res3$bmr)[[1]][[1]][["acc"]]
+    expect_false(isTRUE(all.equal(round(acc, 5), round(acc3, 5))))
   })
 })
