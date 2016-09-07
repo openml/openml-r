87025e36a903df657477e57650b19353e8e226a3
Jakob Bossek <info@jakobbossek.de>
2016-09-08 00:57:43 +0200

    apply changes to tests

--- tests/testthat/test_server_listOMLDataSets.R
+++ tests/testthat/test_server_listOMLDataSets.R
@@ -4,11 +4,11 @@ skip_on_cran()
 
 test_that("listOMLDataSets", {
   exp.names = c("data.id", "status", "format", "name", "MajorityClassSize",
-    "MaxNominalAttDistinctValues", "MinorityClassSize", #"NumBinaryAtts",
-    "NumberOfClasses", "NumberOfFeatures", "NumberOfInstances",
-    "NumberOfInstancesWithMissingValues", "NumberOfMissingValues",
-    "NumberOfNumericFeatures", "NumberOfSymbolicFeatures")
-  
+    "max.nominal.att.distinct.values", "minority.class.size", #"num.binary.atts",
+    ".number.of.classes", ".number.of.features", ".number.of.instances",
+    ".number.of.instances.with.missing.values", ".number.of.missing.values",
+    ".number.of.numeric.features", "number.of.symbolic.features")
+
   for (dsl in list(.listOMLDataSets(limit = 10L), .listOMLDataSets(tag = "study_1", limit = 10L))) {
     expect_data_frame(dsl, col.names = "unique", min.rows = 1)
     #expect_set_equal(names(dsl), exp.names)
@@ -16,12 +16,12 @@ test_that("listOMLDataSets", {
     expect_factor(dsl$status, any.missing = FALSE)
     expect_character(dsl$name, any.missing = FALSE)
   }
-  ds = .listOMLDataSets(NumberOfInstances = c(40, 100), NumberOfFeatures = c(5, 10),
-    NumberOfClasses = 2, NumberOfMissingValues = 0)
-  expect_true(min(ds$NumberOfInstances) >= 40)
-  expect_true(max(ds$NumberOfInstances) <= 100)
-  expect_true(min(ds$NumberOfFeatures) >= 5)
-  expect_true(max(ds$NumberOfFeatures) <= 10)
-  expect_true(unique(ds$NumberOfClasses) == 2)
-  expect_true(unique(ds$NumberOfMissingValues) == 0)
+  ds = .listOMLDataSets(number.of.instances = c(40, 100), number.of.features = c(5, 10),
+    number.of.classes = 2, number.of.missing.values = 0)
+  expect_true(min(ds$number.of.instances) >= 40)
+  expect_true(max(ds$number.of.instances) <= 100)
+  expect_true(min(ds$number.of.features) >= 5)
+  expect_true(max(ds$number.of.features) <= 10)
+  expect_true(unique(ds$number.of.classes) == 2)
+  expect_true(unique(ds$number.of.missing.values) == 0)
 })
