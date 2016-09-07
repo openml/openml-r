05b8b97cc5ce6ea1b3f586818cfcf157b16a3cd4
Jakob Bossek <info@jakobbossek.de>
2016-09-07 23:53:31 +0200

    getOMLDataSet now accepts data.name (and data.version)

--- R/listOMLDataSets.R
+++ R/listOMLDataSets.R
@@ -20,6 +20,7 @@
       data.id = as.integer(xmlValue(children[["did"]])),
       status = xmlValue(children[["status"]]),
       format = xmlValue(children[["format"]]),
+      version = xmlValue(children[["version"]]),
       name = xmlValue(children[["name"]])
     )
     qualities = convertNodeSetToList(children[is.quality], fun = as.integer)
