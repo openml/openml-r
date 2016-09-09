d00a058d19ce9fcf22712528d5c951855b037054
Jakob Bossek <info@jakobbossek.de>
2016-09-09 18:05:29 +0200

    add testcase for #270: check that returned runs have all the requested tags

--- tests/testthat/test_server_listOMLRuns.R
+++ tests/testthat/test_server_listOMLRuns.R
@@ -9,10 +9,16 @@ test_that("listOMLRuns", {
   expect_data_frame(rs, ncols = 7L, min.rows = 50, col.names = "unique")
   expect_true(all(rs$task.id == 2L))
   expect_set_equal(names(rs), exp.names)
-  
+
   # filter runs with tag: study_1
   rs2 = .listOMLRuns(task.id = 2L, tag = "study_1")
   rs3 = .listOMLRuns(task.id = 3L, tag = "study_1")
   rs4 = .listOMLRunEvaluations(task.id = 2:3, tag = "study_1")
   #expect_equal(nrow(rs4), nrow(rs2) + nrow(rs3))
+
+  # check that all returns runs have the desired tag (github issue 270)
+  test.tag = "myspecialtag"
+  rs = .listOMLRuns(tag = test.tag)
+  expect_data_frame(rs, col.names = "unique")
+  expect_true(all(grepl(rs$tags, test.tag)))
 })
