1d55d2d12097171ebf1889b2ca745b63ef76c693
giuseppec <giuseppe.casalicchio@gmail.com>
2016-10-06 14:46:54 +0200

    allow task.type, evaluation.measure and estimation.procedure in task/list

--- R/listOMLTasks.R
+++ R/listOMLTasks.R
@@ -1,11 +1,24 @@
-.listOMLTasks = function(number.of.instances = NULL, number.of.features = NULL,
-  number.of.classes = NULL, number.of.missing.values = NULL,
+.listOMLTasks = function(task.type = NULL, 
+  estimation.procedure = NULL, evaluation.measures = NULL, 
+  number.of.instances = NULL, number.of.features = NULL,
+  number.of.classes = NULL, number.of.missing.values = NULL, 
   tag = NULL, data.name = NULL, data.tag = NULL,
   limit = NULL, offset = NULL, status = "active", verbosity = NULL) {
-
+  
+  estim.proc = listOMLEstimationProcedures(verbosity = 0)
+  eval = listOMLEvaluationMeasures(verbosity = 0)
+  
+  if (!is.null(evaluation.measures)) 
+    assertSubset(evaluation.measures, choices = eval$name)
+  if (!is.null(estimation.procedure)) {
+    assertSubset(estimation.procedure, choices = estim.proc$name)
+    estimation.procedure = estim.proc$est.id[estim.proc$name %in% estimation.procedure]
+  }
+  
   api.call = generateAPICall("json/task/list",
-    number.of.instances = number.of.instances, number.of.features = number.of.features,
-    number.of.classes = number.of.classes, number.of.missing.values = number.of.missing.values,
+    task.type = task.type, number.of.instances = number.of.instances, 
+    number.of.features = number.of.features, number.of.classes = number.of.classes, 
+    number.of.missing.values = number.of.missing.values,
     tag = tag, data.name = data.name, data.tag = data.tag,
     limit = limit, offset = offset, status = status)
 
@@ -13,20 +26,23 @@
 
   res = fromJSON(txt = content, simplifyVector = FALSE)$tasks$task
   input = convertNameValueListToDF(extractSubList(res, "input", simplify = FALSE))
+  # get rid of less interesting stuff
+  input = input[, which(colnames(input)%in%c("source_data", "target_value", "time_limit", "number_samples")):=NULL]
   qualities = convertNameValueListToDF(extractSubList(res, "quality", simplify = FALSE))
   tags = convertTagListToTagString(res)
-
-  # get rid of less interesting stuff
-  #input$source_data = input$target_value = input$time_limit = input$number_samples = NULL
-  input = input[, which(colnames(input)%in%c("source_data", "target_value", "time_limit", "number_samples")):=NULL]
-
-  # include columns for estimation and evaluation if missing
+  # subset according to evaluation measure and estimation procedure
+  ind.eval = ind.estim = rep(TRUE, nrow(input))
+  if (!is.null(evaluation.measures))
+    ind.eval = input$evaluation_measures %in% evaluation.measures 
+  if (!is.null(estimation.procedure))
+    ind.estim = input$estimation_procedure %in% estimation.procedure 
+  
+  # add NA columns for estimation and evaluation if missing
   if (is.null(input$estimation_procedure)) {
     input$estimation_procedure = NA
   } else {
-    estproc = listOMLEstimationProcedures(verbosity = 0L)
-    row.names(estproc) = estproc$est.id
-    input$estimation_procedure = as.character(estproc[input$estimation_procedure , "name"])
+    row.names(estim.proc) = estim.proc$est.id
+    input$estimation_procedure = as.character(estim.proc[input$estimation_procedure , "name"])
   }
   if (is.null(input$evaluation_measures)) input$evaluation_measures = NA_character_
 
@@ -44,8 +60,8 @@
 
   # finally convert _ to . in col names
   names(res) = convertNamesOMLToR(names(res))
-
-  return(res)
+  
+  return(res[ind.estim & ind.eval, ])
 }
 
 #' @title List available OpenML tasks.
@@ -55,7 +71,21 @@
 #' the \code{status} and some describing data qualities.
 #'
 #' @template note_memoise
-#'
+#' 
+#' @param task.type [\code{character(1)}]\cr
+#'   If not \code{NULL}, only tasks belonging to the given task type are listed.
+#'   Use \code{listOMLTaskTypes()$name} to see possible values for \code{task.type}.
+#'   The default is \code{NULL}, which means that tasks with all available task types are listed.
+#' @param estimation.procedure [\code{character}]\cr
+#'   If not \code{NULL}, only tasks belonging the given estimation procedures are listed.
+#'   Use \code{listOMLEstimationProcedures()$name} to see possible values for 
+#'   \code{estimation.procedure}. The default is \code{NULL}, which means that tasks with all 
+#'   available estimation procedures are listed.
+#' @param evaluation.measures [\code{character}]\cr
+#'   If not \code{NULL}, only tasks belonging the given evaluation measures are listed.
+#'   Use \code{listOMLEvaluationMeasures()$name} to see possible values for 
+#'   \code{evaluation.measures}. The default is \code{NULL}, which means that tasks with all 
+#'   available evaluation measures are listed.
 #' @template arg_number.of.instances
 #' @template arg_number.of.features
 #' @template arg_number.of.classes
