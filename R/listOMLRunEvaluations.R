9b3b080dc20dab0c2ca46eb8889a5d94503dcef5
giuseppec <giuseppe.casalicchio@gmail.com>
2017-03-02 15:25:48 +0100

    improve list run evalutaions

--- R/listOMLRunEvaluations.R
+++ R/listOMLRunEvaluations.R
@@ -1,5 +1,6 @@
 .listOMLRunEvaluations = function(task.id = NULL, flow.id = NULL, run.id = NULL,
-  uploader.id = NULL, tag = NULL, limit = NULL, offset = NULL, verbosity = NULL) {
+  uploader.id = NULL, tag = NULL, limit = NULL, offset = NULL, verbosity = NULL, 
+  show.array.measures = FALSE, pretty.flow.names = TRUE) {
 
   if (is.null(task.id) && is.null(flow.id) && is.null(run.id) && is.null(uploader.id) && is.null(tag))
     stop("Please hand over at least one of the following: task.id, flow.id, run.id, uploader.id, tag")
@@ -34,14 +35,26 @@
   # to achive our naming conventions
   colnames(evals) = stri_replace_all_fixed(colnames(evals), "value_", "")
   arr.ind = stri_detect_fixed(colnames(evals), "array_data_")
-  colnames(evals)[arr.ind] = paste0(stri_replace_all_fixed(colnames(evals)[arr.ind], "array_data[_]", ""), "_array")
-
+  colnames(evals)[arr.ind] = paste0(stri_replace_all_fixed(colnames(evals)[arr.ind], "array_data_", ""), "_array")
+  if (!show.array.measures) {
+    evals = evals[,!arr.ind]
+  }
+  
   # convert types (by default all is character)
   #evals = as.data.frame(lapply(evals, type.convert, numerals = "no.loss", as.is = TRUE))
 
   # finally convert _ to . in col names
   names(evals) = convertNamesOMLToR(names(evals))
-
+  
+  flow.source = stri_replace_first(evals$flow.name, regex = "[.].*", replacement = "")
+  flow.source = ifelse(flow.source %in% c("classif", "regr"), "mlr", flow.source)
+  evals = as.data.frame(append(evals, after = which(names(evals) == "flow.name"),
+    values = list(flow.source = flow.source)))
+  
+  if (pretty.flow.names) {
+    evals$flow.name = stri_replace_all(evals$flow.name, regex = ".*[.]|\\(.*\\)", replacement = "")
+  }
+  
   return(evals)
 }
 
@@ -57,6 +70,11 @@
 #' @template note_memoise
 #'
 #' @inheritParams listOMLRuns
+#' @param show.array.measures
+#'  Should measures that return an array (i.e. confusion matrix, predictive accuracy per cv-fold) instead of a single skalar value be shown?
+#' @param pretty.flow.names
+#'  Should the version number of the flow and the prefix that specifies the software be removed?
+#'   
 #' @return [\code{data.frame}].
 #' @family list
 #' @export
