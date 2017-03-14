33a96a3e0c88221e47c6ea8ccf6e2914cb9143ca
giuseppec <giuseppe.casalicchio@gmail.com>
2017-03-14 21:27:58 +0100

    I hate debugging travis

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,13 @@
 library(testthat)
-library(OpenML)
+
+normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache"))
+dir.exists(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache")))
+list.files(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache")))
+list.files(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache")))
+
+getwd()
+list.files(getwd())
+normalizePath(file.path(getwd(), ".."))
+list.files(normalizePath(file.path(getwd(), "..")))
 
 test_check("OpenML")
