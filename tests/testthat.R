11fe510b85f31a0f6b8020f5604e1fa72158bc46
giuseppec <giuseppe.casalicchio@gmail.com>
2017-03-14 22:23:36 +0100

    change test cache

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,3 @@
 library(testthat)
 
-normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache"))
-dir.exists(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache")))
-list.files(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache")))
-list.files(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache")))
-
-getwd()
-list.files(getwd())
-normalizePath(file.path(getwd(), ".."))
-list.files(normalizePath(file.path(getwd(), "..")))
-
-
 test_check("OpenML")
