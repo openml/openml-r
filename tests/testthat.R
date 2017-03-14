da0de18f3c6660306f9c310c301912d3ff631154
giuseppec <giuseppe.casalicchio@gmail.com>
2017-03-14 23:10:30 +0100

    find out testcache location

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,14 @@
 library(testthat)
 
+normalizePath(file.path(find.package("OpenML"), "00_pkg_src", "OpenML", "tests", "cache"))
+list.files(normalizePath(file.path(find.package("OpenML"), "00_pkg_src", "OpenML", "tests", "cache")))
+list.files(normalizePath(file.path(find.package("OpenML"), "00_pkg_src", "OpenML", "tests", "cache", "flow")))
+list.files(normalizePath(file.path(find.package("OpenML"), "00_pkg_src", "OpenML", "tests", "cache", "flow", "2")))
+
+list.files(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache")))
+list.files(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache", "flow")))
+list.files(normalizePath(file.path(find.package("OpenML"), "..", "tests", "cache", "flow", "2")))
+
+identical(Sys.getenv("TRAVIS"), "true")
+
 test_check("OpenML")
