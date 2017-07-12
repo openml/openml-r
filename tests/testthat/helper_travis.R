3d54590cb3a7ce4bb0e641dcd2549c25b00645c4
giuseppec <giuseppe.casalicchio@gmail.com>
2017-07-12 15:13:06 +0200

    fix load_all

--- tests/testthat/helper_travis.R
+++ tests/testthat/helper_travis.R
@@ -7,25 +7,25 @@ if (identical(Sys.getenv("TRAVIS"), "true") || identical(Sys.getenv("APPVEYOR"),
 }
 
 # add flows if they are missing
-flows = listOMLFlows(tag = "mlr_test_flows")
-lrn.list = list(
-  makeLearner("classif.rpart", predict.type = "response"),
-  makeLearner("classif.rpart", predict.type = "prob"),
-  makeLearner("classif.logreg", predict.type = "response"),
-  makeLearner("classif.logreg", predict.type = "prob"),
-  makeLearner("classif.lda"),
-  makeLearner("classif.randomForest"),
-  makeFilterWrapper(makeLearner("classif.randomForest"), fw.perc = 0.5, fw.method = "variance"),
-  makeOversampleWrapper(makeLearner("classif.randomForest"), osw.rate = 1),
-  makeImputeWrapper(makeLearner("classif.randomForest"), class = imputeMedian()),
-  makeOversampleWrapper(makeFilterWrapper(makeLearner("classif.randomForest"), fw.method = "variance"), osw.rate = 1),
-  makeLearner("regr.rpart"),
-  makeLearner("regr.lm")
-)
-lrn.ids = unique(unlist(lapply(lrn.list, function(x) paste0("mlr.", getLearnerId(x)))))
-
-if (nrow(flows) < length(lrn.ids)) {
-  flow.ids = lapply(lrn.list, uploadOMLFlow, tag = "mlr_test_flows")
-}
+# flows = listOMLFlows(tag = "mlr_test_flows")
+# lrn.list = list(
+#   makeLearner("classif.rpart", predict.type = "response"),
+#   makeLearner("classif.rpart", predict.type = "prob"),
+#   makeLearner("classif.logreg", predict.type = "response"),
+#   makeLearner("classif.logreg", predict.type = "prob"),
+#   makeLearner("classif.lda"),
+#   makeLearner("classif.randomForest"),
+#   makeFilterWrapper(makeLearner("classif.randomForest"), fw.perc = 0.5, fw.method = "variance"),
+#   makeOversampleWrapper(makeLearner("classif.randomForest"), osw.rate = 1),
+#   makeImputeWrapper(makeLearner("classif.randomForest"), class = imputeMedian()),
+#   makeOversampleWrapper(makeFilterWrapper(makeLearner("classif.randomForest"), fw.method = "variance"), osw.rate = 1),
+#   makeLearner("regr.rpart"),
+#   makeLearner("regr.lm")
+# )
+# lrn.ids = unique(unlist(lapply(lrn.list, function(x) paste0("mlr.", getLearnerId(x)))))
+#
+# if (nrow(flows) < length(lrn.ids)) {
+#   flow.ids = lapply(lrn.list, uploadOMLFlow, tag = "mlr_test_flows")
+# }
 
 Sys.setenv(NOT_CRAN = "true")
