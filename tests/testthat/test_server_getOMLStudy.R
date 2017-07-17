b22f069e0b519752db7d304a2c6287a18766ca64
Giuseppe Casalicchio <giuseppe.casalicchio@stat.uni-muenchen.de>
2017-07-17 16:24:56 +0200

    benchmarksuite (#384)

--- tests/testthat/test_server_getOMLStudy.R
+++ tests/testthat/test_server_getOMLStudy.R
@@ -5,11 +5,5 @@ test_that("getOMLStudy", {
     study = getOMLStudy(14)
     expect_is(study, "OMLStudy")
     expect_output(print(study), "study_14")
-
-    data.ids = getOMLBenchmarkDataSetIds()
-    expect_set_equal(data.ids, study$data$data.id)
-
-    task.ids = getOMLBenchmarkTaskIds()
-    expect_set_equal(task.ids, study$tasks$task.id)
   })
 })
