e62c2d00d91d3e21ea349cbccab00227285d6a15
giuseppec <giuseppe.casalicchio@gmail.com>
2017-08-11 12:44:12 +0200

    tag was removed

--- tests/testthat/test_server_getOMLStudy.R
+++ tests/testthat/test_server_getOMLStudy.R
@@ -6,7 +6,7 @@ test_that("getOMLStudy", {
     study.by.alias = getOMLStudy("OpenML100")
 
     expect_is(study, "OMLStudy")
-    expect_output(print(study), "study_14")
+    expect_output(print(study), "OpenML100|14")
 
     expect_equal(study, study.by.alias)
   })
