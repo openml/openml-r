16bb5494994ed3c4d420cac626ce270cb442a855
giuseppec <giuseppe.casalicchio@gmail.com>
2017-08-11 11:55:59 +0200

    remove benchmarksuite

--- R/getOMLStudy.R
+++ R/getOMLStudy.R
@@ -2,31 +2,35 @@
 #'
 #' @description
 #' A OpenML study is a collection of OpenML objects with a specific tag defined by the user (i.e. "study_X").
+#' If you create a study through the website \url{https://www.openml.org/new/study}, you can also specify an alias which can be used to access the study.
 #'
-#' @param study.id [\code{character(1)}]\cr
+#' @param study [\code{numeric(1)}|\code{character(1)}]\cr
+#'   Either the id or the alias of a study.
 #' @template arg_verbosity
 #' @return [\code{OMLStudy}].
 #' @family downloading functions
 #' @export
-getOMLStudy = function(study.id = NULL, verbosity = NULL) {
-  study.id = collapseNotScientific(assertIntegerish(study.id, null.ok = TRUE))
-  api.call = generateAPICall(paste0("json/study/", study.id))
+getOMLStudy = function(study = NULL, verbosity = NULL) {
+  #study = collapseNotScientific(assertIntegerish(study, null.ok = TRUE))
+  assert(checkIntegerish(study, null.ok = TRUE), checkString(study, null.ok = TRUE))
+  study = if (is.numeric(study)) collapseNotScientific(study) else study
+  api.call = generateAPICall(paste0("json/study/", study))
 
   content = doAPICall(api.call = api.call, file = NULL, verbosity = verbosity, method = "GET")
   if (is.null(content)) return(data.frame())
-  study = fromJSON(txt = content)$study
+  ret = fromJSON(txt = content)$study
 
   # make proper names
-  names(study) = convertNamesOMLToR(names(study))
-  study = lapply(study, function(x) {
+  names(ret) = convertNamesOMLToR(names(ret))
+  ret = lapply(ret, function(x) {
     if (!is.null(names(x)))
       names(x) = convertNamesOMLToR(names(x))
     return(x)
   })
 
   # convert to numeric values
-  study = rapply(study, type.convert, as.is = TRUE, how = "replace")
-  return(setClasses(study, "OMLStudy"))
+  ret = rapply(ret, type.convert, as.is = TRUE, how = "replace")
+  return(setClasses(ret, "OMLStudy"))
 }
 
 #' @export
--- tests/testthat/test_server_getOMLStudy.R
+++ tests/testthat/test_server_getOMLStudy.R
@@ -3,7 +3,11 @@ context("getOMLStudy")
 test_that("getOMLStudy", {
   with_main_server({
     study = getOMLStudy(14)
+    study.by.alias = getOMLStudy("OpenML100")
+
     expect_is(study, "OMLStudy")
     expect_output(print(study), "study_14")
+
+    expect_equal(study, study.by.alias)
   })
 })
