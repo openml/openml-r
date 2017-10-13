77b3f60fd3c64245ed5cc69b282eb5e765f221c3
giuseppec <giuseppe.casalicchio@gmail.com>
2017-10-13 15:39:06 +0200

    add tests

--- R/chunkOMLlist.R
+++ R/chunkOMLlist.R
@@ -1,14 +1,18 @@
-chunkOMLlist = function(listfun, ..., limit = 100000, server.limit = 10000) {
-  offset = seq(1, limit, by = server.limit)
+chunkOMLlist = function(listfun, ..., total.limit = 100000, chunk.limit = 1000) {
+  assertSubset(as.character(substitute(listfun)), 
+    choices = c("listOMLFlows", "listOMLRuns", "listOMLRunEvaluations",
+      "listOMLDataSets", "listOMLTasks", "listOMLSetup"))
+  
+  offset = seq(0, total.limit - 1, by = chunk.limit)
   res = vector("list", length(offset))
   for (i in seq_along(offset)) {
-    tmp = try(listfun(..., offset = offset[i], limit = server.limit))
+    tmp = try(listfun(..., offset = offset[i], limit = chunk.limit))
     if (!(is.error(tmp) || nrow(tmp) == 0)) {
       res[[i]] = tmp
       if (i == length(offset))
-        return(rbindlist(filterNull(res), fill = TRUE))
+        return(as.data.frame(rbindlist(filterNull(res), fill = TRUE), stringsAsFactors = FALSE))
     } else {
-      return(rbindlist(filterNull(res), fill = TRUE))
+      return(as.data.frame(rbindlist(filterNull(res), fill = TRUE), stringsAsFactors = FALSE))
     }
   }
 }
---
+++ tests/testthat/test_server_chunkOMLlist.R
@@ -0,0 +1,28 @@
+context("chunkOMLlist")
+
+test_that("chunkOMLlist", {
+  expect_chunk = function(listfun, ..., total.limit = 100, chunk.limit = 50) {
+    d1 = listfun(limit = total.limit, ...)
+    d2 = chunkOMLlist(listfun, ..., total.limit = total.limit, chunk.limit = chunk.limit)
+    expect_equal(d1, d2)
+  }
+  
+  # check task
+  expect_chunk(listOMLTasks)
+  
+  # check datasets
+  expect_chunk(listOMLDataSets)
+  
+  # check flows
+  expect_chunk(listOMLFlows)
+  expect_chunk(listOMLFlows, total.limit = 2, chunk.limit = 1)
+  expect_chunk(listOMLFlows, total.limit = 100000, chunk.limit = 10000)
+  
+  # check runs
+  expect_chunk(listOMLRuns, task.id = 59)
+  expect_chunk(listOMLRuns, task.id = 59, total.limit = 2, chunk.limit = 1)
+  
+  # check run evaluations
+  expect_chunk(listOMLRunEvaluations, evaluation.measure = "predictive_accuracy", task.id = 59)
+  expect_chunk(listOMLRunEvaluations, evaluation.measure = "predictive_accuracy", task.id = 59, total.limit = 2, chunk.limit = 1)
+})
