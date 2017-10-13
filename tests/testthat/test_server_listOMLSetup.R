3df6c3f25319e0bb34ad6e57bc2f5a45a5a1a474
giuseppec <giuseppe.casalicchio@gmail.com>
2017-10-13 10:32:30 +0200

    fix test

--- tests/testthat/test_server_listOMLSetup.R
+++ tests/testthat/test_server_listOMLSetup.R
@@ -4,28 +4,28 @@ skip_on_cran()
 
 test_that("listOMLSetup", {
   exp.cols = c("setup.id", "flow.id", "parameter.name", "data.type", "default.value", "value")
-  df = listOMLSetup(limit = 1)
+  df = .listOMLSetup(limit = 1)
   expect_data_frame(df)
   expect_subset(exp.cols, colnames(df))
   expect_true(length(unique(df$setup.id)) == 1)
   
-  df = listOMLSetup(limit = 10)
+  df = .listOMLSetup(limit = 10)
   expect_data_frame(df)
   expect_subset(exp.cols, colnames(df))
   expect_true(length(unique(df$setup.id)) <= 10)
   
   with_main_server({
-    df = listOMLSetup(limit = 10)
+    df = .listOMLSetup(limit = 10)
     expect_data_frame(df)
     expect_subset(exp.cols, colnames(df))
     expect_true(length(unique(df$setup.id)) <= 10)
     
-    df = listOMLSetup(flow.id = 5685)
+    df = .listOMLSetup(flow.id = 5685)
     expect_data_frame(df)
     expect_subset(exp.cols, colnames(df))
     expect_true(unique(df$flow.id) == 5685)
     
-    df = listOMLSetup(setup.id = 9008)
+    df = .listOMLSetup(setup.id = 9008)
     expect_data_frame(df)
     expect_subset(exp.cols, colnames(df))
     expect_true(unique(df$setup.id) == 9008)
