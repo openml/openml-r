668f118cccf5573cf108e543dbac2cb3ea611bd8
Bagrat Ter-Akopyan <bagrat.akopyan@gmail.com>
2019-02-02 23:09:32 +0100

    tryCatch block: return NA if task exists with modified message, else the id of the created task

--- R/uploadOMLTask.R
+++ R/uploadOMLTask.R
@@ -7,21 +7,24 @@
 uploadOMLTask = function(task_type_id, source_data, target_feature, estimation_procedure,
                          evaluation_measures = NULL, tags = NULL, description = NULL,
                          confirm.upload = NULL, verbosity = NULL) {
-  
   desc.file = tempfile(fileext = ".xml")
   on.exit(unlink(desc.file))
   writeOMLTaskXML(task_type_id, source_data, target_feature, estimation_procedure,
                   desc.file, evaluation_measures)
-  
-  showInfo(verbosity, "Uploading data set to server.")
-  
-  res = doAPICall(api.call = "task", method = "POST", file = NULL, verbosity = verbosity,
+  showInfo(verbosity, "Uploading task to server.")
+  res = tryCatch(
+    {
+      response = doAPICall(api.call = "task", method = "POST", file = NULL, verbosity = verbosity,
                      post.args = list(description = upload_file(path = desc.file)))
-  
-  #doc = parseXMLResponse(response, "Uploading dataset", c("upload_data_set", "response"), as.text = TRUE)
-  #task.id = xmlOValI(doc, "/oml:upload_data_set/oml:id")
-  #showInfo(verbosity, "Data set successfully uploaded. Data set ID: %i", data.id)
-  #if (!is.null(tags)) tagOMLObject(data.id, object = "data", tags = tags)
-  #forget(listOMLDataSets)
-  #return(invisible(data.id))
+      doc = parseXMLResponse(response, "Uploading task", c("upload_task", "response"), as.text = TRUE)
+      task.id = xmlOValI(doc, "/oml:upload_task/oml:id")
+      showInfo(verbosity, "Task successfully uploaded. Task ID: %i", task.id)
+      return(invisible(task.id))
+    },
+    error = function(err) {
+      print(paste0(conditionMessage(err), " for data.id = ", source_data))
+      return(invisible(NA))
+    }
+  )
+  return(res)
 }
