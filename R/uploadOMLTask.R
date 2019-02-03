3839c19c5987dd176675cdc9165fefb244a4c2dc
Bagrat Ter-Akopyan <bagrat.akopyan@gmail.com>
2019-02-03 11:12:56 +0100

    removed tryCatch block

--- R/uploadOMLTask.R
+++ R/uploadOMLTask.R
@@ -12,19 +12,13 @@ uploadOMLTask = function(task_type_id, source_data, target_feature, estimation_p
   writeOMLTaskXML(task_type_id, source_data, target_feature, estimation_procedure,
                   desc.file, evaluation_measures)
   showInfo(verbosity, "Uploading task to server.")
-  res = tryCatch(
-    {
-      response = doAPICall(api.call = "task", method = "POST", file = NULL, verbosity = verbosity,
-                     post.args = list(description = upload_file(path = desc.file)))
-      doc = parseXMLResponse(response, "Uploading task", c("upload_task", "response"), as.text = TRUE)
-      task.id = xmlOValI(doc, "/oml:upload_task/oml:id")
-      showInfo(verbosity, "Task successfully uploaded. Task ID: %i", task.id)
-      return(invisible(task.id))
-    },
-    error = function(err) {
-      print(paste0(conditionMessage(err), " for data.id = ", source_data))
-      return(invisible(NA))
-    }
-  )
-  return(res)
+  response = doAPICall(api.call = "task", method = "POST", file = NULL, verbosity = verbosity,
+                 post.args = list(description = upload_file(path = desc.file)))
+
+  #extract task id from response
+  doc = parseXMLResponse(response, "Uploading task", c("upload_task", "response"), as.text = TRUE)
+  task.id = xmlOValI(doc, "/oml:upload_task/oml:id")
+  showInfo(verbosity, "Task successfully uploaded. Task ID: %i", task.id)
+
+  return(invisible(task.id))
 }
