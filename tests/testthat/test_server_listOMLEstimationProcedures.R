7ae2a791a11fe1b70b846d04c6366a81e3783759
giuseppec <giuseppe.casalicchio@gmail.com>
2019-02-22 07:52:17 +0100

    fix #374

--- tests/testthat/test_server_listOMLEstimationProcedures.R
+++ tests/testthat/test_server_listOMLEstimationProcedures.R
@@ -4,8 +4,8 @@ skip_on_cran()
 
 test_that("listOMLEstimationProcedures", {
   df = .listOMLEstimationProcedures()
-  expect_data_frame(df, min.rows = 5, ncols = 3)
-  expect_set_equal(names(df), c("est.id", "task.type", "name"))
+  expect_data_frame(df, min.rows = 5)
+  expect_set_equal(names(df), c("est.id", "task.type", "name", "type", "repeats", "folds", "stratified.sampling", "percentage"))
   expect_true("10-fold Crossvalidation" %in% df$name)
   expect_true("Supervised Classification" %in% df$task.type)
 })
