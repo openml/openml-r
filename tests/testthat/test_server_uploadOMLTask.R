8f9706ae4fb7453209ae894fa070b0303fc3d9dc
Bagrat Ter-Akopyan <bagrat.akopyan@gmail.com>
2019-04-02 09:53:48 +0200

    adjusted tests [ci skip]

--- tests/testthat/test_server_uploadOMLTask.R
+++ tests/testthat/test_server_uploadOMLTask.R
@@ -4,7 +4,7 @@ test_that("uploadOMLTask throws an error if task already exists", {
   with_test_cache({
     # test on gina dataset (id = 41158)
     ds = getOMLDataSet(41158L)
-    gettask = function() { uploadOMLTask(1L, ds$desc$id, ds$target.features, 1L, verbosity = 1) }
+    gettask = function() { uploadOMLTask("Supervised Classification", ds$desc$id, ds$target.features, 1L, verbosity = 1) }
     expect_error(gettask())
   })
 })
@@ -13,7 +13,7 @@ test_that("uploadOMLTask returns an task.id if task successfully created", {
   with_test_cache({
     # test on gina dataset (id = 41158)
     ds = getOMLDataSet(41158L)
-    task.id = uploadOMLTask(1L, ds$desc$id, ds$target.features, 2L)
+    task.id = uploadOMLTask("Supervised Classification", ds$desc$id, ds$target.features, 2L)
     expect_is(task.id, "integer")
     deleteOMLObject(task.id, object = "task")
   })
