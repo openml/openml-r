a63f22d54b5081ca1c9cf29493fed1e4e306ae50
Bagrat Ter-Akopyan <bagrat.akopyan@gmail.com>
2019-09-09 17:03:52 +0200

    add convert oml splits to mlr3

---
+++ tests/testthat/test_local_convertOMLSplitsToMlr3.R
@@ -0,0 +1,25 @@
+context("convertOMLSplitsToMlr3")
+
+test_that("convertOMLSplitsToMlr3", {
+  with_test_cache({
+    task = getOMLTask(59)
+    mlr.task = convertOMLTaskToMlr3(task)$mlr.task
+
+    oml.types = c("crossvalidation", "holdout")
+    mlr.types = c("cv", "holdout")
+
+    for (i in seq_along(oml.types)) {
+      task$input$estimation.procedure[['type']]= oml.types[i]
+      if (oml.types[i] == "holdout") {
+        task$input$estimation.procedure$parameters$percentage = "50"
+      }
+      splits = convertOMLSplitsToMlr3(task$input$estimation.procedure, mlr.task)
+      expect_is(splits, "Resampling")
+      expect_equal(splits$id, mlr.types[i])
+    }
+
+    # pass invalid estim.proc
+    task$input$estimation.procedure$type = "blabla"
+    expect_error(convertOMLSplitsToMlr3(task$input$estimation.procedure, mlr.task), "Unsupported estimation procedure type: blabla")
+  })
+})
